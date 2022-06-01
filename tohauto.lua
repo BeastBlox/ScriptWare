098908e5ef621d6d91e3738bcf4fbad3a0afc737
ducky <106386094+BeastBlox@users.noreply.github.com>
2022-05-31 22:03:02 -0400

    Create tohauto.lua

---
+++ tohauto.lua
@@ -0,0 +1,17 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.tower.sections.finish.FinishGlow.CFrame
+wait(1)
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local Rejoin = coroutine.create(function()
+    local Success, ErrorMessage = pcall(function()
+        TeleportService:Teleport(game.PlaceId, LocalPlayer)
+    end)
+
+    if ErrorMessage and not Success then
+        warn(ErrorMessage)
+    end
+end)
+
+coroutine.resume(Rejoin)
